187558423865fe72e54f06d0a5c7fab351aad68f
Christopher Hicks <visrule@protonmail.com>
2021-01-28 18:18:03 -0800

    Initial Commit

---
+++ StacksLesson/StacksLessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  StacksLessonApp.swift
+//  StacksLesson
+//
+//  Created by Christopher Hicks on 1/28/21.
+//
+
+import SwiftUI
+
+@main
+struct StacksLessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
